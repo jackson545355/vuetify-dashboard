16bc338ef83255d3afcadd68ea1dc8176f0f6615
Trần Minh Nghĩa <nghia.tranminh622002@gmail.com>
2024-06-06 13:55:01 +0700

    Updated: AddProduct, fix Filter in ProductPage

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -8,31 +8,48 @@
       </v-breadcrumbs-item>
     </v-breadcrumbs>
     <div class="addstepcontent">
-      <v-stepper class="step" :items="['Step 1',]">
+      <v-stepper class="step" :items="['Main Infor', 'Link & media', 'Launch']">
         <template v-slot:[`item.1`]>
           <v-card title="We need to some information of new product" flat>
             <v-form>
               <v-card>
                 <v-card-text>
-                  <h3>Basic</h3>
-                  <v-text-field label="Product name (English)" v-model="productNameEn" required></v-text-field>
-                  <v-text-field label="Product name (Japanese)" v-model="productNameJp" required></v-text-field>
-                  <v-text-field label="Short description (English)" v-model="shortDescriptionEn"
-                    required></v-text-field>
-                  <v-text-field label="Short description (Japanese)" v-model="shortDescriptionJp"
-                    required></v-text-field>
-                  <v-textarea label="Full description (English)" v-model="fullDescriptionEn" required></v-textarea>
-                  <v-textarea label="Full description (Japanese)" v-model="fullDescriptionJp" required></v-textarea>
-                  <v-radio-group v-model="status" row>
-                    <template v-slot:label>Status *</template>
+                  <h2>Basic</h2>
+                  <br />
+                  <h4>Product name <span class="redstar">*</span></h4>
+                  <v-text-field label="English" v-model="productNameEn" required
+                    :rules="[v => !!v || 'Product Name is required']"></v-text-field>
+                  <v-text-field label="日本語" v-model="productNameJp" required
+                    :rules="[v => !!v || '製品名は必須です']"></v-text-field>
+                  <h4>Short description <span class="redstar">*</span></h4>
+                  <v-text-field label="English" v-model="shortDescriptionEn" required
+                    :rules="[v => !!v || 'Short description is required']"></v-text-field>
+                  <v-text-field label="日本語" v-model="shortDescriptionJp" required
+                    :rules="[v => !!v || '簡単な説明が必要です']"></v-text-field>
+                  <h4>Full description <span class="redstar">*</span></h4>
+                  <v-textarea label="English" v-model="fullDescriptionEn" required
+                    :rules="[v => !!v || 'Full description is required']"></v-textarea>
+                  <v-textarea label="日本語" v-model="fullDescriptionJp" required
+                    :rules="[v => !!v || '完全な説明が必要です']"></v-textarea>
+                  <h2>Status <span class="redstar">*</span></h2>
+                  <br />
+                  <v-radio-group v-model="status" :rules="[v => !!v || 'Status is required']" row>
                     <v-radio label="Demo" value="demo"></v-radio>
                     <v-radio label="Implementing" value="implementing"></v-radio>
                   </v-radio-group>
-                  <h3>Techniques</h3>
-                  <v-select :items="platformTags" label="Platform Tag *" v-model="platformTag" required></v-select>
-                  <v-select :items="technologyFrameworks" label="Technology Frameworks *" v-model="technologyFramework"
-                    required></v-select>
-                  <v-select :items="tools" label="Tool *" v-model="tool" required></v-select>
+                  <h2>Techniques</h2>
+                  <br />
+                  <h4>Platform Tag <span class="redstar">*</span></h4>
+                  <v-select :items="platformTags" label="Platform Tag" v-model="platformTag" required
+                    :rules="[v => !!v || 'Platform Tag is required']"></v-select>
+                  <h4>Technology Framework <span class="redstar">*</span></h4>
+                  <v-select :items="technologyFrameworks" label="Technology Frameworks" v-model="technologyFramework"
+                    required :rules="[v => !!v || 'Platform Tag is required']"></v-select>
+                  <h4>Tools <span class="redstar">*</span></h4>
+                  <v-select :items="tools" label="Tool" v-model="tool" required
+                    :rules="[v => !!v || 'Platform Tag is required']"></v-select>
+                  <H4 style="color: grey; text-align:center;">Click to below button or "Image & Media" part on tab bar
+                    to add media for product.</H4>
                 </v-card-text>
               </v-card>
             </v-form>
