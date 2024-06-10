80f0bb790fb401e4318b734e2641a09580fab5b7
Trần Minh Nghĩa <nghia.tranminh622002@gmail.com>
2024-06-10 15:00:35 +0700

    bug: MasterItems can't navigate

--- src/views/MasterItems.vue
+++ src/views/MasterItems.vue
@@ -1,45 +1,45 @@
 <template>
   <v-container>
-      <v-row>
-          <v-col>
-              <h1 class="title">Master Items</h1>
-          </v-col>
-      </v-row>
-      <v-tabs v-model="tab" class="tabs">
-          <v-tab>Technologies</v-tab>
-          <v-tab>Tools</v-tab>
-          <v-tab>Platforms</v-tab>
-      </v-tabs>
-      <br/>
-      <v-tabs-items v-model="tab">
-          <v-tab-item>
-              <Technologies />
-          </v-tab-item>
-          <v-tab-item>
-              <Tools />
-          </v-tab-item>
-          <v-tab-item>
-              <Platforms />
-          </v-tab-item>
-      </v-tabs-items>
+    <v-row>
+      <v-col>
+        <h1 class="title">Master Items</h1>
+      </v-col>
+    </v-row>
+    <v-tabs v-model="tab" class="tabs">
+      <v-tab key="technologies">Technologies</v-tab>
+      <v-tab key="tools">Tools</v-tab>
+      <v-tab key="platforms">Platforms</v-tab>
+    </v-tabs>
+    <br/>
+    <v-tabs-items v-model="tab">
+      <v-tab-item key="technologies">
+        <Technologies />
+      </v-tab-item>
+      <v-tab-item key="tools">
+        <Tools />
+      </v-tab-item>
+      <v-tab-item key="platforms">
+        <Platforms />
+      </v-tab-item>
+    </v-tabs-items>
   </v-container>
 </template>
 
 <script>
 import Technologies from '../components/Technologies.vue'
 import Tools from '../components/Tools.vue'
-// import Platforms from '../components/Platforms.vue'
+import Platforms from '../components/Platforms.vue'
 
 export default {
   components: {
-      Technologies,
-      Tools,
-      // Platforms,
+    Technologies,
+    Tools,
+    Platforms,
   },
   data() {
-      return {
-          tab: null,
-      }
+    return {
+      tab: 'technologies', // Chọn tab mặc định là 'technologies'
+    }
   },
 }
 </script>
